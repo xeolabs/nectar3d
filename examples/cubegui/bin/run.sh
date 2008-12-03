dbc944070bffb635419f3eefb5d4a9789083c33f
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2008-12-03 09:26:28 +0000

    Fixed build names

--- examples/cubegui/bin/run.sh
+++ examples/cubegui/bin/run.sh
@@ -1,1 +1,1 @@
-java -Xms10000000 -cp ../lib/nectar3d-1.0.0.jar:../jar/cubegui-1.0.0.jar com.neocoders.cubeGui.app.CubeGUIApp
+java -Xms10000000 -cp ../lib/nectar3d-snapshot.jar:../jar/cubegui-snapshot.jar com.neocoders.cubeGui.app.CubeGUIApp
